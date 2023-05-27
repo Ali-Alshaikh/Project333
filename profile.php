4183e476bee128d8c3db9fde6194d306b0731c4f
Ragad7 <133979482+Ragad7@users.noreply.github.com>
2023-05-27 07:47:09 +0300

    Update profile2.php

--- profile.php
+++ profile.php
@@ -18,45 +18,111 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="profileStyle.css">
     <style>
+     /* Base styles */
         .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: 2px solid #2b2b2b;
-  margin: 2px;
-  background-color: lightblue;
-  padding: 100px;
-  }
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border: 2px solid #2b2b2b;
+            margin: 2px;
+            background-color: lightblue;
+            padding: 100px;
+        }
 
- .container img {
-  width: 200px;
-  height: 200px;
-  object-fit: cover;
-  border-radius: 50%;
-  margin-bottom: 20px;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
- }
+        .container img {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 50%;
+            margin-bottom: 20px;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+        }
 
- .container .form-control {
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 20px;
-  text-align: center;
- }
+        .container .form-control {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 20px;
+            text-align: center;
+        }
+        /* Button styles */
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            font-size: 18px;
+            font-weight: bold;
+            border-radius: 5px;
+            text-decoration: none;
+            transition: background-color 0.3s ease;
+        }
 
- .container .btn {
-  padding: 10px 20px;
-  background-color: #2b2b2b;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  text-decoration: none;
-  font-weight: bold;
- }
+        .btn:hover {
+            background-color: #0069d9;
+        }
 
- .container .btn:hover {
-  background-color: #333;
- }
+
+   /* Media queries */
+   @media only screen and (max-width: 768px) {
+        /* Adjust container  */
+        .container {
+            padding: 50px;
+        }
+        
+        /* Adjust image size  */
+        .container img {
+            width: 150px;
+            height: 150px;
+            margin-bottom: 10px;
+        }
+
+        /* Adjust font size  */
+        .container .form-control {
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+    }
+
+    @media only screen and (max-width: 480px) {
+        /* Adjust container padding  */
+        .container {
+            padding: 30px;
+        }
+
+        /* Adjust image size  */
+        .container img {
+            width: 100px;
+            height: 100px;
+            margin-bottom: 5px;
+        }
+
+        /* Adjust font size  */
+        .container .form-control {
+            font-size: 16px;
+            margin-bottom: 5px;
+        }
+    }
+
+    @media only screen and (max-width: 320px) {
+        /* Adjust container padding  */
+        .container {
+            padding: 20px;
+        }
+
+        /* Adjust image size  */
+        .container img {
+            width: 80px;
+            height: 80px;
+            margin-bottom: 3px;
+        }
+
+        /* Adjust font size */
+        .container .form-control {
+            font-size: 14px;
+            margin-bottom: 3px;
+        }
+    }
+        
     </style>
     <title>user profile</title>
 </head>
