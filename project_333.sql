7036b355ce6c7680a3946590632377785bf845a3
Ali-Alshaikh <alis3348s@gmail.com>
2023-05-27 06:53:18 +0300

    new database

--- project_333.sql
+++ project_333.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 25, 2023 at 05:48 AM
+-- Generation Time: May 27, 2023 at 05:52 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -34,13 +34,6 @@ CREATE TABLE `MCQ_ans` (
   `uniqueNum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `MCQ_ans`
---
-
-INSERT INTO `MCQ_ans` (`id`, `Qid`, `answer`, `uniqueNum`) VALUES
-(1, 59, 'tea', 3);
-
 -- --------------------------------------------------------
 
 --
@@ -59,65 +52,60 @@ CREATE TABLE `questionaire` (
   `uniqueNum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `questionaire`
+-- Table structure for table `scale`
 --
 
-INSERT INTO `questionaire` (`id`, `type`, `question`, `option1`, `option2`, `option3`, `option4`, `qnum`, `uniqueNum`) VALUES
-(55, 'multiple_choice', 'messi age ? ', '35', '34', '36', '37', 1, 1),
-(56, 'multiple_choice', 'mohamed hasan age? ', '12', '17', '20', '22', 2, 2),
-(57, 'multiple_choice', 'fatima hasan age?', '12', '13', '15', '16', 2, 2),
-(58, 'multiple_choice', 'fav shoes? ', 'nike ', 'addidas', 'puma ', 'new balance', 1, 1),
-(59, 'multiple_choice', 'what are drinking? ', 'tea ', 'coffee', 'mango juice ', 'green tea', 2, 3),
-(60, 'multiple_choice', 'say alhamdulliah', 'subhanALLAH ', 'Bismillah', 'alhamdulilah ', 'all praise to allah ', 2, 3),
-(61, 'multiple_choice', 'Alzahra age ? ', '18', '12', '20', '22', 1, 27),
-(62, 'multiple_choice', 'your favorite color? ', 'red', 'blue', 'light blue', 'lime', 2, 26),
-(63, 'multiple_choice', 'your favorite game?', 'football', 'basketball', 'baseBall', 'handBall', 2, 26),
-(64, 'multiple_choice', 'messi age ? ', '12', '22', '223', '25', 1, 13);
+CREATE TABLE `scale` (
+  `id` int(11) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `op1` int(11) NOT NULL,
+  `op2` int(11) NOT NULL,
+  `op3` int(11) NOT NULL,
+  `op4` int(11) NOT NULL,
+  `op5` int(11) NOT NULL,
+  `uniqueNum` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `quiz`
+-- Table structure for table `scale_ans`
 --
 
-CREATE TABLE `quiz` (
-  `quizid` int(255) NOT NULL,
-  `question` varchar(255) NOT NULL,
-  `choice1` varchar(255) NOT NULL,
-  `choice2` varchar(255) NOT NULL,
-  `choice3` varchar(255) NOT NULL,
-  `choice4` varchar(255) NOT NULL,
-  `answer` varchar(255) NOT NULL
+CREATE TABLE `scale_ans` (
+  `id` int(11) NOT NULL,
+  `Qid` int(11) NOT NULL,
+  `answer` int(11) NOT NULL,
+  `uniqueNum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `quiz`
---
-
-INSERT INTO `quiz` (`quizid`, `question`, `choice1`, `choice2`, `choice3`, `choice4`, `answer`) VALUES
-(1, '2+2', '1', '2', '3', '4', '4'),
-(2, '2+1', '1', '2', '3', '4', '2'),
-(3, '1+1', '1', '2', '18', '20', '2');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shortq`
+-- Table structure for table `short`
 --
 
-CREATE TABLE `shortq` (
-  `id` int(255) NOT NULL,
-  `question` varchar(255) NOT NULL,
-  `answer` varchar(255) NOT NULL
+CREATE TABLE `short` (
+  `id` int(11) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `uniqueNum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `shortq`
+-- Table structure for table `short_ans`
 --
 
-INSERT INTO `shortq` (`id`, `question`, `answer`) VALUES
-(1, 'capital city of bahrain?', 'manama');
+CREATE TABLE `short_ans` (
+  `id` int(11) NOT NULL,
+  `qID` int(11) NOT NULL,
+  `answer` varchar(200) NOT NULL,
+  `uniqueNum` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -139,12 +127,13 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `usertype`) VALUES
 (1, '20183438', '11111111', 'regular'),
 (3, 'Ahmed', 'ahmed123', 'admin'),
-(30, 'Ali', '123', 'admin'),
 (4, 'ali000', 'ali', 'admin'),
-(5, 'ali1', '1234512345', 'regular'),
-(28, 'ali11111', '11111111', 'regular'),
+(5, 'ali1', '123', 'regular'),
 (17, 'aliali1234', 'aliali1234', 'regular'),
-(32, 'Fadhel', '123', 'admin');
+(28, 'ali11111', '11111111', 'regular'),
+(30, 'Ali', 'Alis2222', 'admin'),
+(32, 'Fadhel', '123', 'admin'),
+(35, 'A11', 'aaa', 'regular');
 
 -- --------------------------------------------------------
 
@@ -161,41 +150,6 @@ CREATE TABLE `Yes_No` (
   `answers` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `Yes_No`
---
-
-INSERT INTO `Yes_No` (`id`, `question`, `option1`, `option2`, `uniqueNum`, `answers`) VALUES
-(1, 'do you like honey? ', 'Yes', 'No', 1, 'yes'),
-(2, 'messi age ?', 'Yes', 'No', 1, 'yes'),
-(3, 'are you a human?', 'Yes', 'No', 2, ''),
-(4, 'do you know IMAM Husain?', 'Yes', 'No', 2, ''),
-(11, 'are you a human?', 'Yes', 'No', 3, ''),
-(12, 'do you pray everyday ?', 'Yes', 'No', 3, ''),
-(13, 'do you thank god everyday?', 'Yes', 'No', 3, ''),
-(17, 'do you work?', 'Yes', 'No', 6, ''),
-(26, 'do you like pepesi?', 'Yes', 'No', 49, 'No'),
-(27, 'do you like water?', 'Yes', 'No', 49, 'Yes'),
-(28, 'have you said alhamdulliah today?', 'Yes', 'No', 49, 'Yes'),
-(29, 'have you ordered dinner?', 'Yes', 'No', 43, 'Yes'),
-(30, 'have you eaten lunch?', 'Yes', 'No', 43, 'Yes'),
-(31, 'did you say alhamdulliah?', 'Yes', 'No', 43, 'Yes'),
-(32, 'did you pray ?', 'Yes', 'No', 43, 'Yes'),
-(33, 'do you drink coffee?', 'Yes', 'No', 12, ''),
-(34, 'do you drink water?', 'Yes', 'No', 12, ''),
-(35, 'do you wear shoes?', 'Yes', 'No', 12, ''),
-(36, 'are you okay ?', 'Yes', 'No', 16, ''),
-(37, 'are you a human?', 'Yes', 'No', 18, ''),
-(38, 'are you twenty two years old?', 'Yes', 'No', 17, ''),
-(44, 'will you pray at the mosque today?', 'Yes', 'No', 21, ''),
-(45, 'will you train today?', 'Yes', 'No', 21, ''),
-(46, 'will you read holy quran', 'Yes', 'No', 21, ''),
-(47, 'have you drank water?', 'Yes', 'No', 35, ''),
-(48, 'will you drink water?', 'Yes', 'No', 35, ''),
-(49, 'are you a human?', 'Yes', 'No', 28, ''),
-(50, 'are you studying in UOB ?', 'Yes', 'No', 28, ''),
-(51, 'are you sleepy?', 'Yes', 'No', 28, '');
-
 -- --------------------------------------------------------
 
 --
@@ -208,50 +162,6 @@ CREATE TABLE `yes_no_Ans` (
   `answer` varchar(150) NOT NULL,
   `uniqueNum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `yes_no_Ans`
---
-
-INSERT INTO `yes_no_Ans` (`id`, `Qid`, `answer`, `uniqueNum`) VALUES
-(105, 37, 'Yes', 18),
-(106, 37, 'Yes', 18),
-(107, 37, 'Yes', 18),
-(108, 38, 'Yes', 17),
-(109, 38, 'No', 17),
-(110, 38, 'Yes', 17),
-(112, 45, 'No', 21),
-(115, 45, 'Yes', 21),
-(118, 45, 'Yes', 21),
-(121, 45, 'Yes', 21),
-(124, 45, 'Yes', 21),
-(127, 45, 'Yes', 21),
-(130, 48, 'Yes', 35),
-(131, 49, 'Yes', 28),
-(132, 50, 'Yes', 28),
-(133, 51, 'Yes', 28);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ynq`
---
-
-CREATE TABLE `ynq` (
-  `id` int(255) NOT NULL,
-  `question` varchar(255) NOT NULL,
-  `yes` varchar(255) NOT NULL,
-  `no` varchar(255) NOT NULL,
-  `answer` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `ynq`
---
-
-INSERT INTO `ynq` (`id`, `question`, `yes`, `no`, `answer`) VALUES
-(1, 'is 2+2=4', 'yes', 'no', 'yes'),
-(2, 'is 5*10=40', 'yes', 'no', 'no');
 
 --
 -- Indexes for dumped tables
@@ -271,23 +181,39 @@ ALTER TABLE `questionaire`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `quiz`
+-- Indexes for table `scale`
 --
-ALTER TABLE `quiz`
-  ADD PRIMARY KEY (`quizid`);
+ALTER TABLE `scale`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `shortq`
+-- Indexes for table `scale_ans`
 --
-ALTER TABLE `shortq`
+ALTER TABLE `scale_ans`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Qid` (`Qid`);
+
+--
+-- Indexes for table `short`
+--
+ALTER TABLE `short`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `short_ans`
+--
+ALTER TABLE `short_ans`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `qID` (`qID`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`) USING BTREE,
-  ADD UNIQUE KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `id_2` (`id`);
 
 --
 -- Indexes for table `Yes_No`
@@ -303,12 +229,6 @@ ALTER TABLE `yes_no_Ans`
   ADD KEY `forgienKeyLink` (`Qid`);
 
 --
--- Indexes for table `ynq`
---
-ALTER TABLE `ynq`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -316,49 +236,55 @@ ALTER TABLE `ynq`
 -- AUTO_INCREMENT for table `MCQ_ans`
 --
 ALTER TABLE `MCQ_ans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `questionaire`
 --
 ALTER TABLE `questionaire`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
--- AUTO_INCREMENT for table `quiz`
+-- AUTO_INCREMENT for table `scale`
 --
-ALTER TABLE `quiz`
-  MODIFY `quizid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `scale`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
--- AUTO_INCREMENT for table `shortq`
+-- AUTO_INCREMENT for table `scale_ans`
 --
-ALTER TABLE `shortq`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `scale_ans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `short`
+--
+ALTER TABLE `short`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `short_ans`
+--
+ALTER TABLE `short_ans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `Yes_No`
 --
 ALTER TABLE `Yes_No`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT for table `yes_no_Ans`
 --
 ALTER TABLE `yes_no_Ans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
-
---
--- AUTO_INCREMENT for table `ynq`
---
-ALTER TABLE `ynq`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=194;
 
 --
 -- Constraints for dumped tables
@@ -369,6 +295,18 @@ ALTER TABLE `ynq`
 --
 ALTER TABLE `MCQ_ans`
   ADD CONSTRAINT `f1` FOREIGN KEY (`Qid`) REFERENCES `questionaire` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `scale_ans`
+--
+ALTER TABLE `scale_ans`
+  ADD CONSTRAINT `scale_ans_ibfk_1` FOREIGN KEY (`Qid`) REFERENCES `scale` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `short_ans`
+--
+ALTER TABLE `short_ans`
+  ADD CONSTRAINT `short_ans_ibfk_1` FOREIGN KEY (`qID`) REFERENCES `short` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `yes_no_Ans`
